379cd40d04224551ea8da6d3db611d3064790dec
r2d2rigo <r2d2rigo@gmail.com>
2013-03-27 20:13:17 +0100

    Updated sample game to draw textures

--- Minigine.Minigame/Minigame.cpp
+++ Minigine.Minigame/Minigame.cpp
@@ -5,8 +5,52 @@ using namespace Minigine::Math;
 using namespace Minigine::Graphics;
 
 Minigame::Minigame()
-	: Game(GraphicsDevice(800, 600)), spriteBatch(graphicsDevice)
+	: Game(GraphicsDevice(800, 600)), spriteBatch(graphicsDevice), texture(graphicsDevice, 16, 16)
 {
+	byte data[4 * 16 * 16];
+
+	for (int i = 0; i < 16; ++i)
+	{
+		for (int j = 0; j < 16; ++j)
+		{
+			if (i % 2 == 0)
+			{
+				if (j % 2 == 0)
+				{
+					data[i * 64 + j * 4] = 255;
+					data[i * 64 + j * 4 + 1] = 255;
+					data[i * 64 + j * 4 + 2] = 255;
+					data[i * 64 + j * 4 + 3] = 255;
+				}
+				else
+				{
+					data[i * 64 + j * 4] = 0;
+					data[i * 64 + j * 4 + 1] = 0;
+					data[i * 64 + j * 4 + 2] = 0;
+					data[i * 64 + j * 4 + 3] = 255;
+				}
+			}
+			else
+			{
+				if (j % 2 == 0)
+				{
+					data[i * 64 + j * 4] = 0;
+					data[i * 64 + j * 4 + 1] = 0;
+					data[i * 64 + j * 4 + 2] = 0;
+					data[i * 64 + j * 4 + 3] = 255;
+				}
+				else
+				{
+					data[i * 64 + j * 4] = 255;
+					data[i * 64 + j * 4 + 1] = 255;
+					data[i * 64 + j * 4 + 2] = 255;
+					data[i * 64 + j * 4 + 3] = 255;
+				}
+			}
+		}
+	}
+
+	this->texture.SetData(data);
 }
 
 void Minigame::Update(float elapsedTime)
@@ -18,8 +62,10 @@ void Minigame::Draw(float elapsedTime)
 	this->graphicsDevice.Clear(Color::Blue);
 
 	this->spriteBatch.Begin();
-	this->spriteBatch.Draw(Vector2F::Zero, Vector2F(100, 100), Color::Yellow);
-	this->spriteBatch.Draw(Vector2F::Zero, Vector2F(50, 50), Color::Green);
-	this->spriteBatch.Draw(Vector2F::Zero, Vector2F(10, 10), Color::White);
+//	this->spriteBatch.Draw(Vector2F::Zero, Vector2F(100, 100), Color::Yellow);
+//	this->spriteBatch.Draw(Vector2F::Zero, Vector2F(50, 50), Color::Green);
+//	this->spriteBatch.Draw(Vector2F::Zero, Vector2F(10, 10), Color::White);
+	this->spriteBatch.Draw(this->texture, Vector2F::Zero, Vector2F(128, 128), Color::Red);
+	this->spriteBatch.Draw(this->texture, Vector2F::Zero, Color::White);
 	this->spriteBatch.End();
 }
--- Minigine.Minigame/Minigame.hpp
+++ Minigine.Minigame/Minigame.hpp
@@ -2,14 +2,17 @@
 
 #include <Game.hpp>
 #include <Graphics/SpriteBatch.hpp>
+#include <Graphics/Texture2D.hpp>
 
 using Minigine::Game;
 using Minigine::Graphics::SpriteBatch;
+using Minigine::Graphics::Texture2D;
 
 class Minigame : public Game
 {
 private:
 	SpriteBatch spriteBatch;
+	Texture2D texture;
 
 public:
 	Minigame();
