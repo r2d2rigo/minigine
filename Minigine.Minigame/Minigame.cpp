7eabbfb2fce475bb071a05f4afeb9e5214924a08
r2d2rigo <r2d2rigo@gmail.com>
2013-03-27 01:15:31 +0100

    Added usings to sample game.

--- Minigine.Minigame/Minigame.cpp
+++ Minigine.Minigame/Minigame.cpp
@@ -1,8 +1,11 @@
 #include "Minigame.hpp"
 #include <Graphics\SpriteBatch.hpp>
 
+using namespace Minigine::Math;
+using namespace Minigine::Graphics;
+
 Minigame::Minigame()
-	: Game(GraphicsDevice(1024, 768)), spriteBatch(graphicsDevice)
+	: Game(GraphicsDevice(800, 600)), spriteBatch(graphicsDevice)
 {
 }
 
@@ -12,11 +15,11 @@ void Minigame::Update(float elapsedTime)
 
 void Minigame::Draw(float elapsedTime)
 {
-	this->graphicsDevice.Clear(Minigine::Graphics::Color::Blue);
+	this->graphicsDevice.Clear(Color::Blue);
 
 	this->spriteBatch.Begin();
-	this->spriteBatch.Draw(Minigine::Math::Vector2F::Zero, Minigine::Math::Vector2F(100, 100), Minigine::Graphics::Color::Yellow);
-	this->spriteBatch.Draw(Minigine::Math::Vector2F::Zero, Minigine::Math::Vector2F(50, 50), Minigine::Graphics::Color::Green);
-	this->spriteBatch.Draw(Minigine::Math::Vector2F::Zero, Minigine::Math::Vector2F(10, 10), Minigine::Graphics::Color::White);
+	this->spriteBatch.Draw(Vector2F::Zero, Vector2F(100, 100), Color::Yellow);
+	this->spriteBatch.Draw(Vector2F::Zero, Vector2F(50, 50), Color::Green);
+	this->spriteBatch.Draw(Vector2F::Zero, Vector2F(10, 10), Color::White);
 	this->spriteBatch.End();
 }
