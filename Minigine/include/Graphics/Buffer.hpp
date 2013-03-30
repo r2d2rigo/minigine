e88ac091eae637cc87472ed5ed57933ed6cf7a48
r2d2rigo <r2d2rigo@gmail.com>
2013-03-30 02:57:11 +0100

    Added a Buffer base class

---
+++ Minigine/include/Graphics/Buffer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <../Minigine.hpp>
+//#include "Graphics/GraphicsDevice.hpp"
+
+namespace Minigine
+{
+	namespace Graphics
+	{
+		class GraphicsDevice;
+
+		// TODO: should this be exported?
+		class MINIGINE_API Buffer
+		{
+		protected:
+			unsigned int handle;
+			bool dynamic;
+			GraphicsDevice& graphicsDevice;
+
+		public:
+			const unsigned int& GetHandle() { return this->handle; }
+			const bool& IsDynamic() const { return this->dynamic; }
+
+		public:
+			Buffer(const GraphicsDevice& device, bool isDynamic = false)
+				: graphicsDevice(const_cast<GraphicsDevice&>(device)), dynamic(isDynamic)
+			{
+			}
+
+			virtual void SetData(int dataSize, void* data) = 0;
+		};
+	}
+}
