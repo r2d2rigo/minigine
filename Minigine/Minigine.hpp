6c31e8279e30eccfe7144ab938288b590235436c
Rodrigo <r2d2rigo@gmail.com>
2013-04-15 20:08:14 +0200

    Changed MINIGINE_API value for iOS

--- Minigine/Minigine.hpp
+++ Minigine/Minigine.hpp
@@ -6,12 +6,18 @@
 #include <iostream>
 #include <string>
 
-#ifdef MINIGINE_EXPORTS
-	#define MINIGINE_API __declspec(dllexport)
-	#define MINIGINE_EXPIMP
-#else
-	#define MINIGINE_API __declspec(dllimport)
-	#define MINIGINE_EXPIMP extern
+#if WINDOWS
+    #ifdef MINIGINE_EXPORTS
+        #define MINIGINE_API __declspec(dllexport)
+        #define MINIGINE_EXPIMP
+    #else
+        #define MINIGINE_API __declspec(dllimport)
+        #define MINIGINE_EXPIMP extern
+    #endif
+#endif
+
+#if IOS
+    #define MINIGINE_API
 #endif
 
 typedef unsigned char byte;
