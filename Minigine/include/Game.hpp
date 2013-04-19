7d5b969a7a049f2c3eedeeb6740d80f1e67c83bb
Rodrigo <r2d2rigo@gmail.com>
2013-04-20 00:08:55 +0200

    Added iOS window creation/run loop to Game class

--- Minigine/include/Game.hpp
+++ Minigine/include/Game.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#if WINDOWS
 #include <SFML/Window.hpp>
+using sf::Window;
+#endif
+
 #include <../Minigine.hpp>
 #include <Graphics/GraphicsDevice.hpp>
 
-using sf::Window;
 using Minigine::Graphics::GraphicsDevice;
 
 namespace Minigine
@@ -12,8 +15,18 @@ namespace Minigine
 	class MINIGINE_API Game
 	{
 	protected:
+#if WINDOWS
 		Window gameWindow;
+#else
+        
+#endif
 		GraphicsDevice graphicsDevice;
+        static Game* runningInstance;
+        
+    public:
+        // TODO: this singleton has been implemented due to limitations in the application life cycle on iOS.
+        //       This should be removed in the future.
+        static Game* GetRunningInstance() { return runningInstance; }
 
 	public:
 		const GraphicsDevice& GetGraphicsDevice() const { return this->graphicsDevice; }
@@ -23,6 +36,8 @@ namespace Minigine
 
 	public:
 		void Run();
+        // TODO: these used to work as pure virtual functions. Why are they getting called now?
+        virtual void Initialize() = 0;
 		virtual void Update(float elapsedTime) = 0;
 		virtual void Draw(float elapsedTime) = 0;
 	};
--- Minigine/source/Game.cpp
+++ Minigine/source/Game.cpp
@@ -1,5 +1,6 @@
 #include <Game.hpp>
 
+#if WINDOWS
 using sf::Event;
 using sf::VideoMode;
 
@@ -8,6 +9,8 @@ namespace Minigine
 	Game::Game(const GraphicsDevice& device)
 		: graphicsDevice(device)
 	{
+        Game::runningInstance = this;
+        
 		this->gameWindow.create(VideoMode(this->graphicsDevice.GetScreenWidth(), this->graphicsDevice.GetScreenHeight()), std::string());
 
 		// TODO: this should go in GraphicsDevice, but before we need a Window initialized!
@@ -37,6 +40,7 @@ namespace Minigine
 				}
 			}
 
+            // TODO: get a correct timestep
 			Update(0.0f);
 			Draw(0.0f);
 
@@ -44,3 +48,27 @@ namespace Minigine
 		}
 	}
 }
+#endif
+
+// TODO: don't create a whole file for each platform!
+#if IOS
+#import "AppDelegate.hpp"
+
+namespace Minigine
+{
+    Game* Game::runningInstance = nil;
+    
+    Game::Game(const GraphicsDevice& device)
+        : graphicsDevice(device)
+    {
+    }
+    
+    void Game::Run()
+    {
+        @autoreleasepool
+        {
+            UIApplicationMain(0, nil, nil, NSStringFromClass([AppDelegate class]));
+        }
+    }
+}
+#endif
