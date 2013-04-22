96302b2703a560788fd6353aa37cd8659402f25c
Rodrigo <r2d2rigo@gmail.com>
2013-04-22 02:30:40 +0200

    Added minitex project and dependencies

---
+++ Tools/minitex/minitex/main.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <png.h>
+
+int main(int argc, const char * argv[])
+{
+    if (argc < 2)
+    {
+        printf("Bad usage");
+    
+        return -1;
+    }
+    
+    FILE* inputFile = fopen(argv[1], "rb");
+    
+//    fread(header, 1, 8, fp);
+//    if (png_sig_cmp(header, 0, 8))
+//        abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
+//    
+//    
+//    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+//    
+//    if (!png_ptr)
+//        abort_("[read_png_file] png_create_read_struct failed");
+//    
+//    info_ptr = png_create_info_struct(png_ptr);
+//    if (!info_ptr)
+//        abort_("[read_png_file] png_create_info_struct failed");
+//    
+//    if (setjmp(png_jmpbuf(png_ptr)))
+//        abort_("[read_png_file] Error during init_io");
+//    
+//    png_init_io(png_ptr, fp);
+//    png_set_sig_bytes(png_ptr, 8);
+//    
+//    png_read_info(png_ptr, info_ptr);
+//    
+//    width = png_get_image_width(png_ptr, info_ptr);
+//    height = png_get_image_height(png_ptr, info_ptr);
+//    color_type = png_get_color_type(png_ptr, info_ptr);
+//    bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+//    
+//    number_of_passes = png_set_interlace_handling(png_ptr);
+//    png_read_update_info(png_ptr, info_ptr);
+//    
+//    
+//    /* read file */
+//    if (setjmp(png_jmpbuf(png_ptr)))
+//        abort_("[read_png_file] Error during read_image");
+//    
+//    row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
+//    for (y=0; y<height; y++)
+//        row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
+//    
+//    png_read_image(png_ptr, row_pointers);
+    
+    fclose(inputFile);
+    
+    return 0;
+}
+
