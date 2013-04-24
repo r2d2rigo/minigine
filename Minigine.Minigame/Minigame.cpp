85ec2590cd7a1669b527a653013a34777748dad1
Rodrigo <r2d2rigo@gmail.com>
2013-04-25 01:57:29 +0200

    Testing PointBatch in minigame

--- Minigine.Minigame/Minigame.cpp
+++ Minigine.Minigame/Minigame.cpp
@@ -9,11 +9,6 @@ Minigame::Minigame()
 	: Game(GraphicsDevice(960, 640))
 {
     Game::runningInstance = this;
-    
-
-
-	//Minigine::Graphics::EffectTechnique technique("attribute vec3 position; void main() { gl_Position = vec4(position, 1.0); } ", "void main() { gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); } ");
-	//this->effect.AddTechnique("test", technique);
 }
 
 void Minigame::Initialize()
@@ -66,9 +61,11 @@ void Minigame::Initialize()
     NSString* str = [[NSBundle mainBundle] pathForResource:@"ship1" ofType:@"mtx"];
     const char *cfilename=[str UTF8String];
     
-//    this->texture = &Texture2D::FromFile(graphicsDevice, cfilename);
-    this->texture = new Texture2D(graphicsDevice, 16, 16);
-	this->texture->SetData(data);
+    this->texture = &Texture2D::FromFile(graphicsDevice, cfilename);
+//    this->texture = new Texture2D(graphicsDevice, 16, 16);
+//	this->texture->SetData(data);
+    
+    this->pointBatch = new PointBatch(graphicsDevice);
 }
 
 void Minigame::Update(float elapsedTime)
@@ -79,11 +76,16 @@ void Minigame::Draw(float elapsedTime)
 {
 	this->graphicsDevice.Clear(Color::Blue);
 
-	this->spriteBatch->Begin();
-//	this->spriteBatch->Draw(Vector2F::Zero, Vector2F(100, 100), Color::Yellow);
-//	this->spriteBatch->Draw(Vector2F::Zero, Vector2F(50, 50), Color::Green);
-//	this->spriteBatch->Draw(Vector2F::Zero, Vector2F(200, 200), Color::White);
-	this->spriteBatch->Draw(*this->texture, Vector2F::Zero, Vector2F(128, 128), Color::White);
+//	this->spriteBatch->Begin();
+//	this->spriteBatch->Draw(Vector2F::Zero, Vector2F(256, 256), Color::White);
+//	this->spriteBatch->Draw(Vector2F::Zero, Vector2F(128, 128), Color::Yellow);
+//	this->spriteBatch->Draw(Vector2F::Zero, Vector2F(64, 64), Color::Green);
+//	this->spriteBatch->Draw(*this->texture, Vector2F::Zero, Vector2F(40, 51), Color::White);
 //	this->spriteBatch->Draw(this->texture, Vector2F::Zero, Color::White);
-	this->spriteBatch->End();
+//	this->spriteBatch->End();
+
+    this->pointBatch->Begin();
+    this->pointBatch->Draw(Vector2F(100, 100), 1.0f, Color::Red);
+    this->pointBatch->Draw(Vector2F(200, 100), 1.0f, Color::Green);
+    this->pointBatch->End();
 }
--- Minigine.Minigame/Minigame.hpp
+++ Minigine.Minigame/Minigame.hpp
@@ -2,11 +2,13 @@
 
 #include <Game.hpp>
 #include <Graphics/SpriteBatch.hpp>
+#include <Graphics/PointBatch.hpp>
 #include <Graphics/Texture2D.hpp>
 #include <Graphics/Effect.hpp>
 
 using Minigine::Game;
 using Minigine::Graphics::SpriteBatch;
+using Minigine::Graphics::PointBatch;
 using Minigine::Graphics::Texture2D;
 using Minigine::Graphics::Effect;
 
@@ -14,6 +16,7 @@ class Minigame : public Game
 {
 private:
 	SpriteBatch* spriteBatch;
+    PointBatch* pointBatch;
 	Texture2D* texture;
 
 public:
