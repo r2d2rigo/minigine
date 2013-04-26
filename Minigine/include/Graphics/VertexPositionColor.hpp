a52e9c3192970f01cadd75617e65a09d0f6aee1c
r2d2rigo <r2d2rigo@gmail.com>
2013-04-27 01:06:13 +0200

    Removed IVertexType interface for now since virtual methods broke OpenGL drawing

--- Minigine/include/Graphics/VertexPositionColor.hpp
+++ Minigine/include/Graphics/VertexPositionColor.hpp
@@ -1,23 +1,18 @@
 #pragma once
 
 #include <../Minigine.hpp>
-#include "Graphics/IVertexType.hpp"
 #include "Math/Vector3F.hpp"
 #include "Graphics/Color.hpp"
 
 using Minigine::Math::Vector3F;
 using Minigine::Graphics::Color;
-using Minigine::Graphics::IVertexType;
 
 namespace Minigine
 {
 	namespace Graphics
 	{
-		struct MINIGINE_API VertexPositionColor : public IVertexType
-		{
-        private:
-            static const VertexDeclaration& declaration;
-            
+		struct MINIGINE_API VertexPositionColor
+		{            
 		public:
 			Vector3F Position;
 			Color Color;
@@ -31,11 +26,6 @@ namespace Minigine
             VertexPositionColor(const Vector3F& position, const Minigine::Graphics::Color& color)
                 : Position(position), Color(color)
             {
-            }
-            
-            virtual const VertexDeclaration& GetVertexDeclaration() const
-            {
-                return VertexPositionColor::declaration;
             }
 		};
 	}
--- Minigine/include/Graphics/VertexPositionColorTexture.hpp
+++ Minigine/include/Graphics/VertexPositionColorTexture.hpp
@@ -3,22 +3,17 @@
 #include <../Minigine.hpp>
 #include "Math/Vector3F.hpp"
 #include "Graphics/Color.hpp"
-#include "Graphics/IVertexType.hpp"
 
 using Minigine::Math::Vector2F;
 using Minigine::Math::Vector3F;
 using Minigine::Graphics::Color;
-using Minigine::Graphics::IVertexType;
 
 namespace Minigine
 {
 	namespace Graphics
 	{
-		struct MINIGINE_API VertexPositionColorTexture : public IVertexType
-		{
-        private:
-            static const VertexDeclaration& declaration;
-		
+		struct MINIGINE_API VertexPositionColorTexture
+        {		
         public:
 			Vector3F Position;
 			Color Color;
@@ -33,11 +28,6 @@ namespace Minigine
 			VertexPositionColorTexture(const Vector3F& position, const Minigine::Graphics::Color& color, const Vector2F& texCoord)
                 : Position(position), Color(color), TexCoord(texCoord)
             {
-            }
-            
-            virtual const VertexDeclaration& GetVertexDeclaration() const
-            {
-                return VertexPositionColorTexture::declaration;
             }
 		};
 	}
