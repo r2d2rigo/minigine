5eaad7442388cea597154a0122500985fcc092f8
r2d2rigo <r2d2rigo@gmail.com>
2013-04-26 02:27:28 +0200

    VertexDeclaration now uses a std::vector of VertexElement

--- Minigine/include/Graphics/VertexDeclaration.hpp
+++ Minigine/include/Graphics/VertexDeclaration.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <vector>
 #include <../Minigine.hpp>
 #include "VertexElement.hpp"
 
+using std::vector;
 using Minigine::Graphics::VertexElement;
 
 namespace Minigine
@@ -12,10 +14,23 @@ namespace Minigine
 		struct MINIGINE_API VertexDeclaration
 		{
 		private:
-			VertexElement elements;
 			int stride;
+			vector<VertexElement> elements;
+            
+        public:
+            const int& GetStride() const { return this->stride; }
+            const vector<VertexElement>& GetElements() const { return this->elements; }
+            
+        public:
+            VertexDeclaration(const vector<VertexElement>& vertexElements) : elements(vertexElements)
+            {
+                this->stride = 0;
 
-
+                for (vector<VertexElement>::const_iterator iter = vertexElements.begin(); iter != vertexElements.end(); ++iter)
+                {
+                    this->stride += iter->GetSize();
+                }
+            };
 		};
 	}
 }
