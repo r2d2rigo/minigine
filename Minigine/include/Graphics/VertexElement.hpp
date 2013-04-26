7f258d4c19af71d25bb85ff29e2e6a8f6e4fdadc
r2d2rigo <r2d2rigo@gmail.com>
2013-04-26 02:27:40 +0200

    VertexElement accessors

--- Minigine/include/Graphics/VertexElement.hpp
+++ Minigine/include/Graphics/VertexElement.hpp
@@ -18,6 +18,30 @@ namespace Minigine
 			VertexElementUsage::VertexElementUsage usage;
 			int usageIndex;
 			int offset;
+            
+        public:
+            const VertexElementFormat::VertexElementFormat& GetFormat() const { return this->format; }
+            const VertexElementUsage::VertexElementUsage& GetUsage() const { return this->usage; }
+            const int& GetUsageIndex() const { return this->usageIndex; }
+            const int& GetOffset() const { return this->offset; }
+            const int GetSize() const
+            {
+                switch (this->format)
+                {
+                    case VertexElementFormat::Float:
+                    case VertexElementFormat::Color:
+                        return 4;
+                    case VertexElementFormat::Vector2F:
+                        return 8;
+                    case VertexElementFormat::Vector3F:
+                        return 12;
+                }
+            }
+            
+        public:
+            VertexElement(VertexElementFormat::VertexElementFormat elementFormat, VertexElementUsage::VertexElementUsage elementUsage, int elementUsageIndex, int elementOffset) : format(elementFormat), usage(elementUsage), usageIndex(elementUsageIndex), offset(elementOffset)
+            {
+            };
 		};
 	}
 }
