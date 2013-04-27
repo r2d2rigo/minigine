22ceb3d04ca6b14be7c681ebcbcd0587c5ada4e6
r2d2rigo <r2d2rigo@gmail.com>
2013-04-27 02:39:58 +0200

    Added VertexPositionColorSize struct

---
+++ Minigine/include/Graphics/VertexPositionColorSize.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "../Minigine.hpp"
+#include "../Math/Vector3F.hpp"
+#include "Color.hpp"
+
+using Minigine::Math::Vector3F;
+using Minigine::Graphics::Color;
+
+namespace Minigine
+{
+	namespace Graphics
+	{
+		struct MINIGINE_API VertexPositionColorSize
+		{
+		public:
+			Vector3F Position;
+			Color Color;
+            float Size;
+            
+		public:
+            VertexPositionColor()
+                : Position(Vector3F::Zero), Color(Color::White), Size(0.0f)
+            {
+            }
+            
+            VertexPositionColor(const Vector3F& position, const Minigine::Graphics::Color& color, const float& size)
+                : Position(position), Color(color), Size(size)
+            {
+            }
+		};
+	}
+}
